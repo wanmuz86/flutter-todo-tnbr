4c4e07232ac4395158fbdc5fa8c93ee16575f301
Wan Muzaffar <wanmuz86@gmail.com>
2021-06-10 12:02:07 +0800

    add item

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -7,7 +7,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> todos = ["Makan","Beli groceries","Con call"];
+  // A List is actually an Array, it is represented with a [] ,
+  // We retrieve the value of list with number [0], [1] , [2]
+  // We use .length to get how many items in an Array
+  // List<type> // type can be : String, int, dynamic
+
+  // A Map is actually an Object, Dictionary, HashMap, Array Associative
+  // It is represented with a {}
+  // We declare it Map<String,type>  // type can be : String, int, dynamic...
+  // We retrieve the value of Map with it's key  = map["name"]
+  List<Map<String,String>> todos = [
+    {"name":"Makan", "location":"House"},
+{"name":"Beli groceries","location":"Giant"},
+    {"name":"Con call","location":"Office"}
+    ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +39,8 @@ class _HomePageState extends State<HomePage> {
           itemCount: todos.length,
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              title: Text(todos[index]),
+              title: Text(todos[index]["name"]!),
+              subtitle: Text(todos[index]["location"]!),
               trailing: Icon(Icons.chevron_right),
               onTap: (){
                 Navigator.push(

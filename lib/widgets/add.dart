3b334a521b4e0415d6986e37e659403eb89c5911
Wan Muzaffar <wanmuz86@gmail.com>
2021-06-15 10:09:13 +0800

    add item

--- lib/widgets/add.dart
+++ lib/widgets/add.dart
@@ -8,40 +8,42 @@ class AddPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("To do list"),),
-        body:Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: nameEditingController,
-                decoration: InputDecoration(
-                  hintText: "Enter Item name"
+        body:SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: nameEditingController,
+                  decoration: InputDecoration(
+                    hintText: "Enter Item name"
+                  ),
                 ),
-              ),
-              TextField(
-                controller: placeEditingController,
-                  decoration: InputDecoration(
-                      hintText: "Enter place to do "
-                  )
-              ),
-              TextField(
-                controller: descEditingController,
-                  decoration: InputDecoration(
-                      hintText: "Enter description"
-                  )
-              ),
-              TextButton(
-                child: Text("Add"),
-                onPressed: (){
-                  var object = {
-                    "name":nameEditingController.text,
-                    "location":placeEditingController.text,
-                  "description":descEditingController.text
-                  };
-                  Navigator.pop(context, object);
-                },
-              )
-            ],
+                TextField(
+                  controller: placeEditingController,
+                    decoration: InputDecoration(
+                        hintText: "Enter place to do "
+                    )
+                ),
+                TextField(
+                  controller: descEditingController,
+                    decoration: InputDecoration(
+                        hintText: "Enter description"
+                    )
+                ),
+                TextButton(
+                  child: Text("Add"),
+                  onPressed: (){
+                    var object = {
+                      "name":nameEditingController.text,
+                      "location":placeEditingController.text,
+                    "description":descEditingController.text
+                    };
+                    Navigator.pop(context, object);
+                  },
+                )
+              ],
+            ),
           ),
         )
     );
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/add.dart';
 import 'package:todo_app/widgets/detail.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
+
   // A List is actually an Array, it is represented with a [] ,
   // We retrieve the value of list with number [0], [1] , [2]
   // We use .length to get how many items in an Array
@@ -16,8 +20,30 @@ class _HomePageState extends State<HomePage> {
   // It is represented with a {}
   // We declare it Map<String,type>  // type can be : String, int, dynamic...
   // We retrieve the value of Map with it's key  = map["name"]
-  List<Map<String,String>> todos = [
+
+  /// List<dynamic> -> A list of anything
+  List<dynamic> todos = [
     ];
+
+  // This is mobile when the page is loaded lifecycle..
+  // It is almost similar to document.ready in jquery..
+  // Mobile app has more lifecycle than normal web..
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadTodos();
+  }
+
+  void loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? todoString = prefs.getString("todos");
+    var todoList = jsonDecode(todoString!);
+    setState(() {
+      todos = todoList;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +52,26 @@ class _HomePageState extends State<HomePage> {
         IconButton(onPressed: () async {
          var result =  await Navigator.push(context,
          MaterialPageRoute(builder: (context)=> AddPage()));
-         setState(() {
-           todos.add(result);
+
+         // I create a copy of current array
+         List<dynamic> tempArray = todos;
+         // I add the data in the copy of array
+         tempArray.add(result);
+
+         // Encode
+         String todosString = jsonEncode(tempArray);
+
+
+         // Save the data inside Shared Preference...
+         final prefs = await SharedPreferences.getInstance();
+         prefs.setString("todos", todosString);
+
+         setState(()  {
+           todos = tempArray;
+
          });
+
+
         },
             icon: Icon(Icons.add)
         )
@@ -55,4 +98,6 @@ class _HomePageState extends State<HomePage> {
       )
       );
   }
+
+
 }
